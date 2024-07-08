8b242f65934b4105558bc44a48db2d52acc2eeb9
sania587 <124341621+sania587@users.noreply.github.com>
2024-07-08 15:38:08 +0500

    vertical nav bar of map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Dashboard from './pages/Dashboard.jsx';
+import About from './pages/Dashboard.jsx';
+import Analytics from './pages/Dashboard.jsx';
+import Comment from './pages/Dashboard.jsx';
+import Product from './pages/Dashboard.jsx';
+import ProductList from './pages/Dashboard.jsx';
+
+import VerticalNav from './Vertical-nav/vertical-nav';
+
 function Map() {
     return (
-        <div>
-            Map
-        </div>
+      <div>
+        <h1>Home</h1>
+      
+      <VerticalNav>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </VerticalNav>
+      </div>
     )
 }
 
