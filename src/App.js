3d58564e17aa47c8e6bedf13f6b3a8b1c75dbacc
Haris Sohail <harissohail.cs@gmail.com>
2024-07-08 10:07:04 +0500

    Init map component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Map from './components/Map';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/map' element={<Map />} />
         </Routes>
       </BrowserRouter>
     </div>
