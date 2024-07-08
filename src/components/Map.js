3de2f2d4cd1e6571162000d46569aa239e113659
Haris Sohail <harissohail.cs@gmail.com>
2024-07-08 11:31:57 +0500

    Add header to the map page

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Logo from '../layout/Logo'
+import HeaderMapPage from '../layout/HeaderMapPage'
 
 function Map() {
     return (
         <div>
             <Logo />
+            <HeaderMapPage />
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        colorMapHeaderBG: '#dde8f0',
+      },
+    },
   },
   plugins: [],
 }
