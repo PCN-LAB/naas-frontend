fe83b19a4c1dad8093a3e9e90a0b25e53b8d2010
Haris Sohail <harissohail.cs@gmail.com>
2024-08-07 11:06:12 +0500

    Make keywords show responive

--- src/layout/KeyWord.js
+++ src/layout/KeyWord.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function KeyWord({ keyWord }) {
     return (
-        <div className='px-5 flex items-center justify-center border-2 border-colorSearchButton text-colorSearchButton rounded-3xl m-2 text-2xl bg-gray-100 font-semibold'>
-            {keyWord}
+        <div className='w-fit h-fit'>
+            <div className='text-sm p-1 md:px-5 md:py-2 flex items-center justify-center border-2 border-colorSearchButton text-colorSearchButton rounded-3xl m-1 md:m-2 md:text-2xl bg-gray-100 font-semibold'>
+                {keyWord}
+            </div>
         </div>
     )
 }
